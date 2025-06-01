5e61689c393d9b716804fd242673fe7eda0ba609
OseiasOlimpio <oseiasolimpio96@gmail.com>
2025-06-01 09:40:33 -0300

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,31 @@
-function menuShow(){
-	let menuMobile = document.querySelector('.mobile-menu');
-	if(menuMobile.classList.contains('open')){
-		menuMobile.classList.remove('open');
-		document.querySelector('.icon').src = 'img/menu_white_36dp.svg';
-	}else{
-		menuMobile.classList.add('open');
-		document.querySelector('.icon').src = "img/close_white_36dp.svg";
-	}
+// menu responsivo
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "img/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "img/close_white_36dp.svg";
+  }
+}
+
+// dados do formulario
+function enviaFormulario(event) {
+  e.preventDefault();
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const telefone = document.getElementById("telefone").value;
+  const projeto = document.getElementById("projeto").value;
+
+  const mensagem = `Olá, me chamo Nome: ${nome} \n E-mail: ${email} \n Telefone para contato: ${telefone} \n Entrei em contato para solicitar o orçamento do seguinte projeto: ${projeto}.`;
+
+  const numeroWhatsApp = "87991097719";
+
+  const urlWhatsapp = `https://api.whatsapp.com/send?phone=${numeroWhatsApp}&text=${encodeURIComponent(
+    mensagem
+  )}`;
+  window.open(urlWhatsapp, "_blank");
+
+  document.getElementById("formContato").addEventListener("submit", enviaFormulario);
 }
